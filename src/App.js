bbf14e4b7ccf083ce0043e5694cf77913d91ec89
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-09 18:04:01 +0500

    southgroup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Catalog from './components/Catalog'
-// import Header from './components/Header'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <>
-    {/* <Header /> */}
+    <Header />
     <Catalog />
     </>
   )
