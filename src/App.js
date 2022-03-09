cf7fa9c5ab85b83290a5c1eab67d37358bd4c576
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-03-09 17:44:58 +0500

    southgroup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Catalog from './components/Catalog'
-import Header from './components/Header'
+// import Header from './components/Header'
 
 const App = () => {
   return (
--- src/components/Catalog.jsx
+++ src/components/Catalog.jsx
@@ -1,129 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-const Catalog = () => {
-    return (
-        <>
-            <div className="pop-categories mt-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12">
-                            <h2><span>П</span>опулярный каталог</h2>
-                        </div>
-                        <div className="col-12 d-flex mt-4">
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/taxeometr-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Тахеометры</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/gnss-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">GNSS приемники</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/nivelir.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Нивелиры</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/lazerniy-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="280px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Лазерные дальномеры</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-12 d-flex pop-categories mt-4">
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/teodolit-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Теодолиты</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/trassoiskatel-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Трассоискатели</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                            <div className="col-3 pop-category">
-                                <div className="card">
-                                    <Link to='/'>
-                                        <img
-                                            src="/img/acsessuary-category.webp"
-                                            alt=""
-                                            className="w-100"
-                                            height="300px"
-                                        />
-                                        <div className="card-body">
-                                            <h5 className="card-title text-center">Аксессуары</h5>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-cube";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCube, Pagination } from "swiper";
+
+export default function App() {
+  return (
+    <>
+      <Swiper
+        effect={"cube"}
+        grabCursor={true}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        pagination={true}
+        modules={[EffectCube, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img alt="qwe" src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img alt="qwe" src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img alt="qwe" src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img alt="qwe" src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </>
+  );
 }
-
-export default Catalog
